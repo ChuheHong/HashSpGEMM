e0f4a5df5a82d2a0279dfddc42f3b376addb8d88
洪楚河 <hongchuhe@hongchuhedeMacBook-Air.local>
2024-01-09 20:53:22 +0800

    change the structure of directory

--- spgemm_example/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    fprintf(f,"hello!\n");
-    return 0;
-}
--- spmv_mpi/spmv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-module purge
-module load GCC/8.3.0
-module load mpich/mpi-x-gcc8.3.0
-time yhrun -N 1 -n 5 -p thcp1 ./spmv
-
-# yhbatch -N 1 -n 5 -p thcp1 ./spmv.sh
